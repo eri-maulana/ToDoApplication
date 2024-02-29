76bd7313472278e1db650db48545144ef0b392e9
eri-maulana <maulanabouble11@gmail.com>
2024-02-29 14:16:55 +0700

    1. Membuat Template

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>To Do Application</title>
+</head>
+
+<body>
+   <h1>To Do Application</h1> <br>
+
+   <form action="" method="post">
+      <label>Apa Kegiatanmu Hari ini?</label> <br>
+      <input type="text" name="todo" id="">
+      <button type="submit">Simpan</button>
+   </form>
+
+   <ul>
+      <li>
+         <input type="checkbox" name="todo" id="cek">
+         <label for="cek"> To do 1</label>
+         <a href="#">hapus</a>
+      </li>
+      <li>
+         <input type="checkbox" name="todo" id="cek">
+         <label for="cek"> To do 1</label>
+         <a href="#">hapus</a>
+      </li>
+      <li>
+         <input type="checkbox" name="todo" id="cek">
+         <label for="cek"> To do 1</label>
+         <a href="#">hapus</a>
+      </li>
+      <li>
+         <input type="checkbox" name="todo" id="cek">
+         <label for="cek"> To do 1</label>
+         <a href="#">hapus</a>
+      </li>
+   </ul>
+</body>
+
+</html>
